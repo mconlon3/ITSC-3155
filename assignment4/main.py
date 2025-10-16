82597852e176e6e8f555731ede8b2128fb4dccd7
mconlon3 <mbconlon28@gmail.com>
2025-10-16 16:40:13 -0400

    finished part 1

--- assignment4/main.py
+++ assignment4/main.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class Item(BaseModel):
+class Item(BaseModel): 
     name: str
     price: float
     is_offer: Union[bool, None] = None
