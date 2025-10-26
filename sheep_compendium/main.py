e69bc4dc92241252d11838ff613214802c54648c
mconlon3 <mbconlon28@gmail.com>
2025-10-26 10:46:17 -0400

    add

---
+++ sheep_compendium/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from models.db import db
+from models.models import Sheep
+app = FastAPI()
+@app.get("/sheep/{id}", response_model=Sheep)
+def read_sheep(id: int):
+    return db.get_sheep(id)
